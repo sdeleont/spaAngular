90e04a89958ab0950c3e66e773242be301769bc7
sdeleont <storon@MINFIN.DI.GOBIERNO>
2018-06-18 16:18:47 -0600

    Clase 57

--- spa/src/app/app.module.ts
+++ spa/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//Rutas
+import{app_routing} from './app.routes'
+//Servicios
+import {HeroesService} from './services/heroes.service'
+//componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
@@ -13,12 +18,16 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- spa/src/app/components/heroes/heroes.component.ts
+++ spa/src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HeroesService, Heroe} from '../../services/heroes.service';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-heroes',
@@ -6,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes:Heroe[]=[];
 
-  ngOnInit() {
+  constructor(private _heroesService:HeroesService,
+              private router:Router
+  ) { 
+    //console.log("Constructor");
   }
 
+  //es bastante utilizado cuando ya esta toda la pagina renderizada
+  ngOnInit() {
+    
+    this.heroes=this._heroesService.getHeroes()
+    //console.log(this.heroes);
+  }
+
+  verHeroe(idx:number){
+    this.router.navigate( ['/heroe',idx] )
+  }
 }
